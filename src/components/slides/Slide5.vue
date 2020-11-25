c9347ae1e935972e018847abd5f1616bed861f5b
cvlvxi <cvlvxi@outlook.com>
2020-11-25 16:54:59 +1100

    update

--- src/components/slides/Slide5.vue
+++ src/components/slides/Slide5.vue
@@ -5,28 +5,15 @@
           <p class="small"> Something discovered that you can use to take forward into the future</p>
       </section>
       <section>
-        Cup of Coffee = $4.00
-      </section>
-      <section>
-        2x Coffees a day = $8.00
-      </section>
-      <section>
-        5x Days you have coffee = $40.00
-      </section>
-      <section>
+        Cup of Coffee = $4.00 <br/>
+        2x Coffees a day = $8.00 <br/>
+        5x Days you have coffee = $40.00 <br/>
         52x Weeks of Coffee (520 coffees) = <span class="red">$2080.00</span>
       </section>
       <section>
         Moka Pot = $104.95 at Myers <br/>
-        <img width="200px" height="200px" src="https://myer-media.com.au/wcsstore/MyerCatalogAssetStore/images/50/514/4231/500/14/195127660/195127660_1_720x928.jpg"/>
-      </section>
-      <section>
-        Coffee Beans 1kg = $60 = 120 cups of coffee
-      </section>
-      <section>
-        How many 1kg coffee bags needed per year?<br/> = 520 / 120 = 8.6 = 5 bags
-      </section>
-      <section>
+        Coffee Beans 1kg = $60 = 120 cups of coffee <br/>
+        How many 1kg coffee bags needed per year?<br/> = 520 / 120 = 8.6 = 5 bags  <br/>
         5 bags * $60 + 104.95 =  <span class="green">$404.95</span>
       </section>
       <section>
